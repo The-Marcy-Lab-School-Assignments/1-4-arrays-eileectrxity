f61ae538245d620ba4606a0fa2f5c7e5e9801bfe
electrxety <er191712@gmail.com>
2024-04-10 15:32:27 -0400

    solved question 1

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,5 +1,15 @@
-const addToFrontOrBack = () => {
+//Lesson 1-4 Assignment: Arrays by Eileen
+
+//Question 1: a function that takes 3 args: an array, arr, a value of any type, value, and a boolean, isFront. insert the value as either the first or last value in the arr, depending on the boolean, isFront. function should mutate the array and return nothing
+const addToFrontOrBack = (arr, value, isFront ) => {
+  if (isFront === false) { //used npm test to figure out if isFront should start with false or true
+    arr.push(value); //if the given boolean is false, add the given value to the end of the array
+  } else arr.unshift(value); //if the given boolean is true, add the given value to the front of the array
+  console.log(arr) //printing the mutated array to the console without using return
 };
+
+addToFrontOrBack([1,2,3, 4, 5], 6, true); //[ 6, 1, 2, 3, 4, 5 ]
+addToFrontOrBack([1,2,3], 4, false); //[ 1, 2, 3, 4 ]
 
 const reverseString = () => {
 };
