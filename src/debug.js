4dc9f18c2a2b18b41d40c44f73c018357b664571
electrxety <er191712@gmail.com>
2024-04-10 21:52:45 -0400

    solved question 12

--- src/debug.js
+++ src/debug.js
@@ -10,9 +10,20 @@ const clearArr = (arr) => {
   arr.splice(0, arr.length); //starting from index 0, effectively mutates and deletes an entire given array by making the delete count the length of the array
 };
 
+console.log(clearArr([1, 2, 3])) // undefined
+
+//Question 12: original broken function- not supposed to mutate the given array; just want the first item of the array. fix to pass the tests
+// const getFirstItem = (array) => {
+//   return array.shift(); //.shift() mutates an array so doesn't work
+// };
+
+//debugged
 const getFirstItem = (array) => {
-  return array.shift();
+    return array[0]; //more simple than the below, accesses the first element (at index 0) of a given array without mutating anything
+  // return array.slice(0, 1); //gets an array copy of the first element in a given array
 };
+
+console.log(getFirstItem([9, 2, 3])) // 9
 
 module.exports = {
   clearArr,
